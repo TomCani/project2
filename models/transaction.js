0004ddbe7deb55020e8a42e96208b6501cad6cfd
Jabruzzese321 <jabruzzese321@gmail.com>
2018-12-08 11:23:06 -0500

    Small edits and joins

--- models/transaction.js
+++ models/transaction.js
@@ -1,22 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-    var Transaction = sequelize.define("Transaction", {
-      fund_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      amount: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-      },
-    });
-    return Transaction;
-  };
+  var Transaction = sequelize.define("Transaction", {
+    fundId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  // Transaction.belongsTo("User"); // Will add companyId to user
+
+  return Transaction;
+};
